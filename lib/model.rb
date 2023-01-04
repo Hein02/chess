baacabc6c1d11c9c94cc2ff2f228e7de5152f3e7
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-03 21:58:28 -0800

    Add Model#select_pc
    To retrieve piece from the board, by calling Board#find_pc.

--- lib/model.rb
+++ lib/model.rb
@@ -5,12 +5,16 @@ require_relative 'board'
 # This class acts as a central command center of a Chess game.
 #
 class Model
-  def initialize(board)
-    @board = board
+  def initialize(brd)
+    @brd = brd
   end
 
   def self.new_game
     brd = Board.init_setup
     Model.new(brd)
+  end
+
+  def select_pc(sqr)
+    @brd.find_pc(sqr)
   end
 end
