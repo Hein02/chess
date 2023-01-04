e1d449b9c9654f6fda31cc546c90f18955a41d28
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-03 20:52:03 -0800

    Add unicode characters lookup for chess pieces

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+# unicode chess-piece symbols look-up
+#
+PCS_SYMS = {
+  black: {
+    K: "\u2654",
+    Q: "\u2655",
+    R: "\u2656",
+    B: "\u2657",
+    N: "\u2658",
+    P: "\u2659"
+  },
+  white: {
+    K: "\u265a",
+    Q: "\u265b",
+    R: "\u265c",
+    B: "\u265d",
+    N: "\u265e",
+    P: "\u265f"
+  }
+}.freeze
+
 # This class represents pieces.
 #
 class Piece
