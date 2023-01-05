ef43d159a65bb3af8b4b93290f3056b3bf07a1a0
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-04 21:07:54 -0800

    Refactor Queen tests

--- spec/queen.spec.rb
+++ spec/queen.spec.rb
@@ -4,6 +4,19 @@ require_relative '../lib/queen'
 
 describe Queen do
   subject(:queen) { described_class.new(:w) }
+  let(:paths) do
+    {
+      N: %i[d2 d3 d4 d5],
+      E: %i[e1 f1 g1 h1],
+      W: %i[c1 b1 a1],
+      S: [],
+      NE: %i[e2 f3 g4 h5],
+      NW: %i[c2 b3 a4],
+      SE: [],
+      SW: []
+    }
+  end
+
   describe '#collect_paths' do
     it 'collects paths for all eight directions' do
       paths = queen.collect_paths(:d1, {})
@@ -38,17 +51,7 @@ describe Queen do
 
     it 'returns all the empty sqrs' do
       sqrs = { d5: pc, h1: enemy_pc }
-      movement = {
-        N: %i[d2 d3 d4 d5],
-        E: %i[e1 f1 g1 h1],
-        W: %i[c1 b1 a1],
-        S: [],
-        NE: %i[e2 f3 g4 h5],
-        NW: %i[c2 b3 a4],
-        SE: [],
-        SW: []
-      }
-      moves = queen.find_moves(movement, sqrs)
+      moves = queen.find_moves(paths, sqrs)
       expected = %i[d2 d3 d4 e1 f1 g1 c1 b1 a1 e2 f3 g4 h5 c2 b3 a4]
       expect(moves).to eq(expected)
     end
@@ -59,17 +62,7 @@ describe Queen do
 
     it 'returns sqrs with enemy pieces on them' do
       sqrs = { d5: enemy_pc, h1: enemy_pc }
-      movement = {
-        N: %i[d2 d3 d4 d5],
-        E: %i[e1 f1 g1 h1],
-        W: %i[c1 b1 a1],
-        S: [],
-        NE: %i[e2 f3 g4 h5],
-        NW: %i[c2 b3 a4],
-        SE: [],
-        SW: []
-      }
-      captures = queen.find_captures(movement, sqrs)
+      captures = queen.find_captures(paths, sqrs)
       expected = %i[d5 h1]
       expect(captures).to eq(expected)
     end
@@ -77,17 +70,7 @@ describe Queen do
     context 'when there is no enemy' do
       it 'returns empty arr' do
         sqrs = {}
-        movement = {
-          N: %i[d2 d3 d4 d5],
-          E: %i[e1 f1 g1 h1],
-          W: %i[c1 b1 a1],
-          S: [],
-          NE: %i[e2 f3 g4 h5],
-          NW: %i[c2 b3 a4],
-          SE: [],
-          SW: []
-        }
-        captures = queen.find_captures(movement, sqrs)
+        captures = queen.find_captures(paths, sqrs)
         expect(captures).to be_empty
       end
     end
