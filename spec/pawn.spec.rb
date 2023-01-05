48cf15c00a42fd0c7f2ed371fbd993f12377d703
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-05 00:40:09 -0800

    Remove unnecessary tests

--- spec/pawn.spec.rb
+++ spec/pawn.spec.rb
@@ -6,30 +6,21 @@ describe Pawn do
   subject(:pawn) { described_class.new(:w) }
 
   describe '#add_one_sq' do
-    context 'when it is white' do
-      it 'adds extra one sq for north dir' do
-        paths = {
-          N: [:d2],
-          NE: [:e3],
-          NW: [:c3]
-        }
-        actual = pawn.add_one_sq(paths)
-        expected = %i[d2 d3]
-        expect(actual[:N]).to eq(expected)
-      end
+    it 'adds extra one sq on vertical direction' do
+      paths = {
+        N: [:d2],
+        NE: [:e3],
+        NW: [:c3]
+      }
+      actual = pawn.add_one_sq(paths)
+      expected = %i[d2 d3]
+      expect(actual[:N]).to eq(expected)
     end
+  end
 
-    context 'when it is black' do
-      it 'adds extra one sq for south dir' do
-        paths = {
-          S: [:d7],
-          SE: [:e6],
-          SW: [:c6]
-        }
-        actual = pawn.add_one_sq(paths)
-        expected = %i[d7 d6]
-        expect(actual[:S]).to eq(expected)
-      end
-    end
+  describe '#movement' do
+    let(:piece) { double('piece', { clr: :w }) }
+
+    context 'when there is a piece'
   end
 end
