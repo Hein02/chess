215463a8fa3193382d98d70c932065ad222d5b15
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-05 06:38:02 -0800

    Make Rook#castling_sq readable

--- lib/rook.rb
+++ lib/rook.rb
@@ -5,6 +5,8 @@ require_relative 'piece'
 # This class represents a rook.
 #
 class Rook < Piece
+  attr_reader :castling_sq
+
   def initialize(clr, id = :R)
     super(clr, id)
     @news = %i[N E W S]
