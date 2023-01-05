11f6504348383879fdd86df1db35f15f567cd6ef
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-04 21:11:51 -0800

    Make Rook child of Queen
    Add @news for horizontal and vertical traversing
    as it cannot traverse diagonally as a queen.

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'piece'
+require_relative 'queen'
 
 # This class represents a rook.
 #
-class Rook < Piece
+class Rook < Queen
   def initialize(clr, id = :R)
     super(clr, id)
+    @news = %i[N E W S]
   end
 end
