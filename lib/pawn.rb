741bd9f01399626e8818e4ee03530ab2aac33deb
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-04 22:45:28 -0800

    Add Pawn#movement
    Different from Piece#movement in a way that
    it requires to add one sq in file path if it is a first move.

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -10,6 +10,15 @@ class Pawn < King
     @news = clr == :w ? %i[NE N NW] : %i[SE S SW]
   end
 
+  def movement(cur_sq, sqrs)
+    paths = collect_paths(cur_sq, sqrs)
+    pawn_paths = add_one_sq(paths) if @first_move == true
+    {
+      moves: find_moves(pawn_paths, sqrs),
+      captures: find_captures(pawn_paths, sqrs)
+    }
+  end
+
   def add_one_sq(paths)
     paths.each_with_object({}) do |(dir, path), hash|
       one_sq = find_path(path[0], dir) { |_, cnt| cnt == 1 }
