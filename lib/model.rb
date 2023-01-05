00709785f61ce960bcc89accdef633aed84a52a8
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-05 04:57:26 -0800

    Add Model#in_check?

--- lib/model.rb
+++ lib/model.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'board'
+require_relative 'player'
 
 # This class acts as a central command center of a Chess game.
 #
@@ -81,8 +82,24 @@ class Model
   end
 
   # Player in check
+  def in_check?
+    dbles = create_dbles
+    dbles.any? do |dbl|
+      movement = dbl.movement(@cur_p.king_sqr, sqrs)
+      captures = movement[:captures]
+      dbl_exists?(dbl, captures)
+    end
+  end
+
   def create_dbles
-    [Queen, Rook, Bishop, Knight, Pawn, King].map { |dbl| dbl.new(@cur_p.color) }
+    [Queen, Rook, Bishop, Knight, Pawn, King].map { |dbl| dbl.new(@cur_p.clr) }
+  end
+
+  def dbl_exists?(dbl, captures)
+    captures.any? do |sqr|
+      found = select_pc(sqr)
+      found && found.clr != @cur_p.clr && found.id == dbl.id
+    end
   end
 
   # TODO: check if the king can make castling move
@@ -127,33 +144,36 @@ w_p = Pawn.new(:w)
 b_p = Pawn.new(:b)
 
 brd = Board.new(Board.empty_sqrs)
-mdl = Model.new(brd)
+w_player = Player.new(:w, 'a')
+b_player = Player.new(:b, 'b')
+mdl = Model.new(brd, w_player, b_player)
 
-mdl.place_pc(b_p, :g7)
-mdl.place_pc(w_p, :f5)
-mdl.place_pc(b_p, :e6)
+# mdl.place_pc(b_p, :g7)
+# mdl.place_pc(w_p, :f5)
+# mdl.place_pc(b_p, :e6)
 
-two_sq = mdl.select_pc(:g7)
-from = :g7
-to = :g5
-mdl.move_pc(from, to)
+# two_sq = mdl.select_pc(:g7)
+# from = :g7
+# to = :g5
+# mdl.move_pc(from, to)
 
-# Checking against rules
-puts mdl.first_move?(two_sq)
-puts mdl.two_sq_adv?(from, to)
-adj_e_pwn = mdl.find_adj_e_pwn(two_sq, :g5)
-# Allowing en_passant move to enemy pawn
-adj_e_pwn&.update_en_pas_sq(to)
+# # Checking against rules
+# puts mdl.first_move?(two_sq)
+# puts mdl.two_sq_adv?(from, to)
+# adj_e_pwn = mdl.find_adj_e_pwn(two_sq, :g5)
+# # Allowing en_passant move to enemy pawn
+# adj_e_pwn&.update_en_pas_sq(to)
 
-en_pas_pwn = mdl.select_pc(:f5)
-puts en_pas_pwn.movement(:f5, mdl.sqrs)
+# en_pas_pwn = mdl.select_pc(:f5)
+# puts en_pas_pwn.movement(:f5, mdl.sqrs)
 
-# making en_passant move
-mdl.move_pc(:f5, :g6)
-mdl.remove_pwn_behind(en_pas_pwn)
-en_pas_pwn.update_en_pas_sq(nil)
-puts mdl.select_pc(:g5)
+# # making en_passant move
+# mdl.move_pc(:f5, :g6)
+# mdl.remove_pwn_behind(en_pas_pwn)
+# en_pas_pwn.update_en_pas_sq(nil)
+# puts mdl.select_pc(:g5)
 
-
-
-# selected = mdl.select_pc(:f5)
+# in check
+mdl.place_pc(w_k, :e1)
+mdl.place_pc(b_k, :d3)
+puts mdl.in_check?
