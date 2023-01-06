86d181341b45c348d45710862cfe4b57d00f5501
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-06 04:19:57 -0800

    Create Controller

---
+++ lib/controller.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require_relative 'model'
+require_relative 'view'
+
+# This class handles user's inputs, and also acts as a mediator between Model and View.
+#
+class Controller
+  def initialize(model, view)
+    @model = model
+    @view = view
+  end
+
+  def self.start
+    options = <<~HEREDOC
+      [1] New game.
+      [2] Load game
+    HEREDOC
+    option = user_input { puts options }
+    option == '2' ? Controller.load_game : Controller.new_game
+  end
+
+  def self.user_input
+    yield if block_given?
+    gets.chomp
+  end
+
+  def self.new_game
+    w_player_name = user_input { print 'Enter White player\'s name: ' }
+    b_player_name = user_input { print 'Enter Black player\'s name: ' }
+    model = Model.new_game(w_player_name, b_player_name)
+    view = View.new
+    Controller.new(model, view)
+  end
+
+  def self.load_game
+
+  end
+
+  def play
+    loop do
+      display_brd
+      display_cur_p_name
+      select_n_move
+      return puts 'Checkmate' if checkmate?
+    rescue GameError => e
+      puts e.message
+    end
+  end
+
+  def display_brd(selected: nil, movement: {})
+    @view.update(sqrs: sqrs, selected: selected, movement: movement)
+    @view.board
+  end
+
+  def sqrs
+    @model.sqrs
+  end
+
+  def display_cur_p_name
+    @view.cur_p_name(cur_p_name)
+  end
+
+  def cur_p_name
+    @model.cur_p_name
+  end
+
+  def select_n_move
+    from = Controller.user_input { print 'Select a piece: ' }.to_sym
+    movement = select_pc(from)
+    display_brd(selected: from, movement: movement)
+    to = Controller.user_input { print 'Move: ' }.to_sym
+    move_pc(from, to, movement)
+  end
+
+  def select_pc(from)
+    selected_pc = @model.select_pc(from)
+    @model.find_movement(selected_pc, from)
+  end
+
+  def move_pc(from, to, movement)
+    @model.move_pc(from, to, movement)
+  end
+
+  def checkmate?
+    @model.checkmate?
+  end
+end
+
+ctrl = Controller.new_game
+ctrl.play
